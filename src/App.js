fa21ab3e350f1a2ba10b7c9dd17afd22a951c875
Youcef <youcef.zahariou@gmail.com>
2019-04-18 03:40:58 +0100

    setting up the react project to start on the animation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Illustration from './components/Illustration'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Illustration/>
+      </div>
+    );
+  }
+}
+
+export default App;
