3489ae2dac9005fadcddb373893d0664e0ac4a9d
Youcef <youcef.zahariou@gmail.com>
2019-10-29 10:57:39 +0100

    done the slide animation

--- src/screens/Slide/index.js
+++ src/screens/Slide/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Carousel from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
