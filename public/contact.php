f6950e4dc6e506f569284e7d6116342cf9cfd480
Youcef <youcef.zahariou@gmail.com>
2019-10-30 19:47:04 +0100

    configuring email sending

--- public/contact.php
+++ public/contact.php
@@ -3,7 +3,7 @@
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) && is_string($_POST['name']) && is_string($_POST['email']) && is_string($_POST['message'])) {
     $to      = 'youcef.zahariou@gmail.com';
     $subject = 'Mail from ' . $_POST['name'];
-    $message =  'Email : ' . $_POST['email'] . ' <br>';
+    $email =  'Email : ' . $_POST['email'] . ' <br>';
     $message .=  'Message : ' . $_POST['message'];
     $headers = 'MIME-Version: 1.0' . "\r\n" .
         'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
@@ -13,7 +13,8 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) 
 
     mail($to, $subject, $message, $headers);
     header('Content-Type: application/json');
-    echo json_encode(['status' => 'success']);
+    echo json_encode(['status' => 'success', "to" => $to, "subject" => $subject, "email" => $email, "message" => $message]);
+    // echo json_encode(['status' => 'success']);
 } else {
     header('Content-Type: application/json');
     echo json_encode(['status' => 'error']);
