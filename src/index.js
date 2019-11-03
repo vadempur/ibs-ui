e96f5c20817f4252b4c86d6030dd85d478b92f8e
Zahariou Youcef <youcef.zahariou@gmail.com>
2019-11-03 18:17:29 +0100

    added the google analytics for pageviews on the website

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {BrowserRouter as Router} from "react-router-dom"
 import * as serviceWorker from './serviceWorker';
 
 import ReactGA from 'react-ga';
-import { GOOGLE_ANALYTICS_TRACKING_ID } from './helpers/constants';
+import { GOOGLE_ANALYTICS_TRACKING_ID, BASE_URL } from './helpers/constants';
 ReactGA.initialize(GOOGLE_ANALYTICS_TRACKING_ID);
-ReactGA.pageview('/homepage');
+ReactGA.pageview(BASE_URL);
 
 ReactDOM.render( <Router><App /></Router> , document.getElementById('root'));
 
